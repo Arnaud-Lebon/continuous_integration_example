641f6fb1bd2712e8150f587a0cb6211c8d7fe710
lebon <lebon.arnaud0@gmail.com>
2021-01-12 20:42:16 +0100

    test

--- sum_test.py
+++ sum_test.py
@@ -4,9 +4,10 @@ import pytest
 
 @pytest.mark.parametrize('numbers, output', [
     ([1, 2, 3], 6),
-    ([1, 2, 3, 4, 5], 15)
+    ([1, 2, 3, 4, 5], 15),
+    ([1, 2, 3, 4], 10)
+    # Ajoutez un test pour que la somme des éléments de la liste soit égale à 10
 ])
-# Ajoutez un test pour que la somme des éléments de la liste soit égale à 10
 def test_mysum(numbers, output):
     assert mysum(numbers) == output
     assert (len(numbers) % 2) == 1
